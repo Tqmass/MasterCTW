15d281581fdf0042e79404d74d854846186196a2
Tqmass <54636540+Rqmennn@users.noreply.github.com>
2024-06-05 23:50:33 +0200

    Starting Files

---
+++ src/main/java/dev/tqmass/ctw/MasterCTW.java
@@ -0,0 +1,19 @@
+package dev.tqmass.ctw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MasterCTW extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+}
